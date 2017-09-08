53ca441794012d797db3827646bbf6b1bc03a409
D-gitlong <chulong.0223@qq.com>
2017-09-08 17:30:16 +0800

    vue 收银系统

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# vue-
+﻿# vue-pos
 实现vue的添加商品，结账的系统
+
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import pos from '@/components/page/Pos'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'pos',
+      component: pos
+    }
+  ]
+})
